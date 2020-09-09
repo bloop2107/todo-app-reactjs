13dadec0682fbed81afca22178732cbc87e87eb7
jackymad06@gmail.com <jackymad06@gmail.com>
2020-09-09 10:57:09 +0700

    Updates

---
+++ precache-manifest.0febba219ac64ac34e396ffda3891364.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "735e42a7a44858b8dcb1fe884a152c79",
+    "url": "/todo-app-reactjs/index.html"
+  },
+  {
+    "revision": "bb16ea45cc1047d7adfb",
+    "url": "/todo-app-reactjs/static/css/main.1cc73c12.chunk.css"
+  },
+  {
+    "revision": "83695359a73039d6331d",
+    "url": "/todo-app-reactjs/static/js/2.f3d9b370.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-reactjs/static/js/2.f3d9b370.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb16ea45cc1047d7adfb",
+    "url": "/todo-app-reactjs/static/js/main.fbcae0a0.chunk.js"
+  },
+  {
+    "revision": "7af7518ad63c20fb4764",
+    "url": "/todo-app-reactjs/static/js/runtime-main.03ccbecb.js"
+  }
+]);
